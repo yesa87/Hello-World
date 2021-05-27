cc66ac00a2f2967d5c3042cf32b3ca0926ae4fcd
yesa87 <schadi.sabri@web.de>
2021-05-27 11:18:55 +0200

    I am learning with my teacher Osman

--- cypress/integration/embed.spec.js
+++
@@ -1,9 +0,0 @@
-/* global cy */
-describe('Embed', () => {
-  it('Should render the Carbon editor but no toolbar', () => {
-    cy.visit('/embed')
-
-    cy.get('.export-container').should('be.visible')
-    cy.get('.export-menu-container').should('not.exist')
-  })
-})
--- cypress/support/index.js
+++
@@ -1,3 +0,0 @@
-/* global cy */
-// import '@applitools/eyes-cypress/commands'
-export const editorVisible = () => cy.get('.editor').should('be.visible')
